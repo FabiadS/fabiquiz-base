d32db504bd087d35b269eefe329b0165d2a3631c
Fabiola Spredemann <fabispredemann@gmail.com>
2021-01-27 18:32:06 -0300

    Primeiro dia finalizado

---
+++ pages/index.js
@@ -0,0 +1,54 @@
+import styled from 'styled-components'
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackground';
+
+/*const BackgroundImage = styled.div`
+  background-image: url(${db.bg});
+  flex: 1;
+  background-size: cover;
+  background-position: center;
+ `; */
+
+ export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width:500px){
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
+
+export default function Home() { //componente que representa a nossa pag
+  return (
+    <QuizBackground backgroundImage = {db.bg}>
+      <QuizContainer>
+        <Widget>
+            <Widget.Header>
+            <h1>Stranger Things</h1>
+            </Widget.Header>
+          <Widget.Content>
+
+            <p>Teste seus conhecimentos sobre a série Stranger Things e veja se você prestou atenção na série!</p>
+          </Widget.Content>
+        
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da Galera</h1>
+
+            <p>Teste seu conhecimento em outros temas</p>
+          </Widget.Content>
+        </Widget>
+        <Footer/>
+      </QuizContainer>
+      <GitHubCorner projectUrl ="https://github.com/FabiadS"/>
+    </QuizBackground>
+  );  
+}
